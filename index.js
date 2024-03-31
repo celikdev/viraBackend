d6f71f67d81cddd8a2504b0e3de304be2cec07f8
Hakan <52114496+celikdev@users.noreply.github.com>
2024-03-31 14:59:42 +0300

    heroku deploy

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { MainRouter } from "./routes/index.js";
+import { connectDB } from "./components/connectDB.js";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.json("Vira - Backend");
+});
+
+app.use("/api", MainRouter);
+
+connectDB();
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
